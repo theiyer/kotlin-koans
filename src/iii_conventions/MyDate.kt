07245e45d7771f84b3135d3b452ebd644a076193
Prakash Iyer <theiyer@gmail.com>
2017-06-11 15:34:24 -0400

    Conventions tests all successfully completed.

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,25 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import com.sun.org.apache.regexp.internal.RE
+import javax.swing.ImageIcon
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+    override operator fun compareTo(other: MyDate): Int {
+        // println("this=" + this.toString() + ", that=" + other.toString())
+        if (this.year < other.year) return -1
+        else if (this.year > other.year) return 1
+        if (this.month < other.month) return -1
+        else if (this.month > other.month) return 1
+        if (this.dayOfMonth < other.dayOfMonth) return -1
+        else if (this.dayOfMonth > other.dayOfMonth) return 1
+        return 0
+    }
+
+    operator fun  inc(): MyDate = nextDay()
+
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +27,32 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateIterator(var current: MyDate, val endInclusive: MyDate): Iterator<MyDate> {
+    override fun hasNext(): Boolean {
+        return current <= endInclusive;
+    }
+
+    override fun next(): MyDate {
+        val returnDate: MyDate = current
+        current++
+        return returnDate
+    }
+}
+
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : Iterable<MyDate>, ClosedRange<MyDate> {
+    override operator fun iterator(): Iterator<MyDate> {
+        var iterator: Iterator<MyDate> = DateIterator(start, endInclusive)
+        return iterator;
+    }
+}
+
+
+// operator fun DateRange.contains(date: MyDate): Boolean = ((start.compareTo(date) <= 0) && (endInclusive.compareTo(date) >= 0))
+
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = this.addTimeIntervals(timeInterval, 1)
+
+operator fun MyDate.plus(ti: RepeatedTimeIntervals): MyDate = this.addTimeIntervals(ti.timeInterval, ti.times)
+
+class RepeatedTimeIntervals(val timeInterval: TimeInterval, val times:Int)
+
+operator fun TimeInterval.times(times: Int): RepeatedTimeIntervals = RepeatedTimeIntervals(this, times)
--- src/iii_conventions/_31_Invoke_.kt
+++ src/iii_conventions/_31_Invoke_.kt
@@ -3,7 +3,11 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable {
+    var nInvokes: Int = 0
+    operator fun invoke():Invokable {nInvokes++; return this}
+    fun getNumberOfInvocations():Int = nInvokes;
+}
 
 fun todoTask31(): Nothing = TODO(
     """
@@ -14,6 +18,6 @@ fun todoTask31(): Nothing = TODO(
     references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+//    todoTask31()
+    return invokable()()()().getNumberOfInvocations()
 }
